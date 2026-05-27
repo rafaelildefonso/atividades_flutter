e0f55415350d70f589ee9eadb395dad59fca89ea
RAFAEL_ILDEFONSO_FURTADO <12402010@OMEGAB43.BARROCA>
2026-05-27 11:54:37 -0300

    improve ui

--- desafio/lib/pages/login_page.dart
+++ desafio/lib/pages/login_page.dart
@@ -27,74 +27,67 @@ class _TelaLoginState extends State<TelaLogin> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.withAlpha(20),
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.all(200.0),
-          child: Container(
-            decoration: BoxDecoration(
-                color: Colors.lightBlueAccent.withAlpha(100),
-                borderRadius: BorderRadius.circular(20)),
-            child: Padding(
-              padding: EdgeInsets.all(20.0),
-              child: Form(
-                child: ListView(
+          padding: const EdgeInsets.all(100.0),
+          child: Form(
+            child: ListView(
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Row(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Login",
-                          style: TextStyle(
-                              fontWeight: FontWeight.w600, fontSize: 35),
-                        ),
-                      ],
+                    Text(
+                      "Login",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600, fontSize: 35),
                     ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    TextFormField(
-                      controller: email,
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(
-                          icon: Icon(Icons.email),
-                          hintText: "email@email.com",
-                          labelText: "Digite seu email",
-                          border: OutlineInputBorder(),
-                          filled: true,
-                          fillColor: Colors.white),
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    TextFormField(
-                      controller: senha,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          icon: Icon(Icons.lock),
-                          hintText: "email@email.com",
-                          labelText: "Digite sua senha",
-                          border: OutlineInputBorder(),
-                          filled: true,
-                          fillColor: Colors.white),
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    ElevatedButton(
-                        style:
-                            ElevatedButton.styleFrom(minimumSize: Size(0, 60)),
-                        onPressed: () {
-                          _validarLogin();
-                        },
-                        child: Text(
-                          "Entrar",
-                          style: TextStyle(
-                              fontWeight: FontWeight.w700, fontSize: 19),
-                        )),
                   ],
                 ),
-              ),
+                SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  controller: email,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                      icon: Icon(Icons.email),
+                      hintText: "email@email.com",
+                      labelText: "Digite seu email",
+                      border: OutlineInputBorder(),
+                      filled: true,
+                      fillColor: Colors.white),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  controller: senha,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      icon: Icon(Icons.lock),
+                      hintText: "email@email.com",
+                      labelText: "Digite sua senha",
+                      border: OutlineInputBorder(),
+                      filled: true,
+                      fillColor: Colors.white),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(minimumSize: Size(0, 60),backgroundColor: Colors.blueAccent),
+                    onPressed: () {
+                      _validarLogin();
+                    },
+                    child: Text(
+                      "Entrar",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w700, fontSize: 19,color: Colors.white),
+                    )),
+              ],
             ),
           ),
         ),
